84bb3b9afffe2856caaa809e7db6c61f3cdc60f5
jaido88 <engineerj2000@gmail.com>
2024-04-10 21:40:54 +0100

    fixes

--- src/utils/flagsmith.ts
+++ src/utils/flagsmith.ts
@@ -1,5 +1,7 @@
 import Flagsmith from "flagsmith-nodejs";
 
+console.log('FLAGSMITH_KEY:', process.env.FLAGSMITH_KEY);
+
 const flagsmith = new Flagsmith({
   environmentKey: process.env.FLAGSMITH_KEY as string,
   requestTimeoutSeconds: 60,
