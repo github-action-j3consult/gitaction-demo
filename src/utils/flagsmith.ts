8e1b4ce20dcf423d34857f8ed6599cd6a5d8673f
jaido88 <engineerj2000@gmail.com>
2024-04-11 10:22:13 +0100

    ttt

--- src/utils/flagsmith.ts
+++ src/utils/flagsmith.ts
@@ -1,10 +1,10 @@
-import Flagsmith from "flagsmith-nodejs";
+//import Flagsmith from "flagsmith-nodejs";
 
 console.log("FLAGSMITH_KEY:", process.env.FLAGSMITH_KEY);
 
-const flagsmith = new Flagsmith({
-  environmentKey: process.env.FLAGSMITH_KEY as string,
-  requestTimeoutSeconds: 60,
-});
+//const flagsmith = new Flagsmith({
+//  environmentKey: process.env.FLAGSMITH_KEY as string,
+//  requestTimeoutSeconds: 60,
+//});
 
-export default flagsmith;
+//export default flagsmith;
